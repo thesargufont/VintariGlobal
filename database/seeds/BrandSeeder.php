98edf08dff1d4028ee7421c2f1a07bbd03c8e97a
Yoel Putra <natbudiono8@gmail.com>
2023-02-25 01:04:04 +0700

    developYoel: add field name english in country

--- database/seeds/BrandSeeder.php
+++ database/seeds/BrandSeeder.php
@@ -10,37 +10,37 @@ class BrandSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('brands')->insert([
-            'name'          => 'LAYS',
-            'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('brands')->insert([
-            'name'          => 'NESTLE',
-            'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('brands')->insert([
-            'name'          => 'REXONA',
-            'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('brands')->insert([
-            'name'          => 'CHITATO',
-            'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
+        // DB::table('brands')->insert([
+        //     'name'          => 'LAYS',
+        //     'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('brands')->insert([
+        //     'name'          => 'NESTLE',
+        //     'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('brands')->insert([
+        //     'name'          => 'REXONA',
+        //     'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('brands')->insert([
+        //     'name'          => 'CHITATO',
+        //     'image_path'    => 'BRAND'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
     }
 }
--- database/seeds/CountrySeeder.php
+++ database/seeds/CountrySeeder.php
@@ -11,61 +11,61 @@ class CountrySeeder extends Seeder
      */
     public function run()
     {
-        DB::table('countries')->insert([
-            'name'          => 'INDONESIA',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('countries')->insert([
-            'name'          => 'BELANDA',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('countries')->insert([
-            'name'          => 'JEPANG',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('countries')->insert([
-            'name'          => 'KOREA',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('countries')->insert([
-            'name'          => 'MALAYSIA',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('countries')->insert([
-            'name'          => 'POLANDIA',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
-        DB::table('countries')->insert([
-            'name'          => 'SPANYOL',
-            'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
-            'created_by'    => 1,
-            'created_at'    => Carbon::now(),
-            'updated_by'    => 1,
-            'updated_at'    => Carbon::now()
-        ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'INDONESIA',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'BELANDA',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'JEPANG',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'KOREA',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'MALAYSIA',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'POLANDIA',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
+        // DB::table('countries')->insert([
+        //     'name'          => 'SPANYOL',
+        //     'image_path'    => 'COUNTRY'.Carbon::now()->format('Y/m/d').'/',
+        //     'created_by'    => 1,
+        //     'created_at'    => Carbon::now(),
+        //     'updated_by'    => 1,
+        //     'updated_at'    => Carbon::now()
+        // ]);
     }
 }
